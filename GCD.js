5b026de59b32c4110ee6631e8511a992e529ed9d
Mohamed Hosam <mohamed.hosam2310@gmail.com>
2025-11-20 02:38:28 +0200

    Added simple  GCD program

---
+++ GCD.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+
+
+const r1 = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+r1.question("Enter the number: ", answer => {
+    numbers = answer.split(" ").map(Number);
+    if (numbers.length !== 2 || numbers.some(isNaN)) {
+        console.log("Please enter exactly two valid numbers separated by a space.");
+        r1.close();
+        return;
+    }
+    const [a, b] = numbers;
+    console.log(`the gcd between the first number ${a} and the second number ${b} is: ${gcd(a, b)} `);
+    r1.close();
+
+});
+
+
+
+
+function gcd(a, b) {
+    var m = Math.max(a, b);
+    var n = Math.min(a, b);
+    var r = m % n;
+
+    if (r === 0) {
+        return n;
+    } else {
+        return gcd(n, r);
+    }
+}
